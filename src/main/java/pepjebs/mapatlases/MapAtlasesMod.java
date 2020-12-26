a942ef3535a2087c925686497b1d1f5b17060b12
rohn <nrohn@tx.rr.com>
2020-12-25 22:26:03 -0600

    Craft atlas with filled map

--- src/main/java/pepjebs/mapatlases/MapAtlasesMod.java
+++ src/main/java/pepjebs/mapatlases/MapAtlasesMod.java
@@ -3,18 +3,26 @@ package pepjebs.mapatlases;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.recipe.RecipeType;
+import net.minecraft.recipe.SpecialCraftingRecipe;
+import net.minecraft.recipe.SpecialRecipeSerializer;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import pepjebs.mapatlases.item.MapAtlasItem;
+import pepjebs.mapatlases.recipe.MapAtlasCreateRecipe;
 
 public class MapAtlasesMod implements ModInitializer {
 
     public static final String MOD_ID = "map_atlases";
 
-    public static final MapAtlasItem MAP_ATLAS = new MapAtlasItem(new Item.Settings().group(ItemGroup.MISC));
+    public static final MapAtlasItem MAP_ATLAS = new MapAtlasItem(new Item.Settings().group(ItemGroup.MISC).maxCount(1));
+
+    public static SpecialRecipeSerializer<MapAtlasCreateRecipe> MAP_ATLAS_RECIPE_SERIALIZER;
 
     @Override
     public void onInitialize() {
+        MAP_ATLAS_RECIPE_SERIALIZER = Registry.register(Registry.RECIPE_SERIALIZER,
+                new Identifier(MOD_ID, "crafting_atlas"), new SpecialRecipeSerializer<>(MapAtlasCreateRecipe::new));
         Registry.register(Registry.ITEM, new Identifier(MOD_ID,"atlas"), MAP_ATLAS);
     }
 }
---
+++ src/main/java/pepjebs/mapatlases/recipe/MapAtlasCreateRecipe.java
@@ -0,0 +1,70 @@
+package pepjebs.mapatlases.recipe;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.FilledMapItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.map.MapState;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.SpecialCraftingRecipe;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+import pepjebs.mapatlases.MapAtlasesMod;
+import pepjebs.mapatlases.item.MapAtlasItem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MapAtlasCreateRecipe extends SpecialCraftingRecipe {
+
+    public MapAtlasCreateRecipe(Identifier id) {
+        super(id);
+    }
+
+    @Override
+    public boolean matches(CraftingInventory inv, World world) {
+        ArrayList<ItemStack> items = new ArrayList<>();
+        for(int i = 0; i < inv.size(); i++) {
+            if (!inv.getStack(i).isEmpty()) {
+                items.add(inv.getStack(i));
+            }
+        }
+        if (items.size() == 3) {
+            List<Item> items1 = items.stream().map(ItemStack::getItem).collect(Collectors.toList());
+            return items1.containsAll(Arrays.asList(Items.FILLED_MAP, Items.CARTOGRAPHY_TABLE, Items.BOOK));
+        }
+        return false;
+    }
+
+    @Override
+    public ItemStack craft(CraftingInventory inv) {
+        ItemStack mapItemStack = null;
+        for(int i = 0; i < inv.size(); i++) {
+            if (inv.getStack(i).isItemEqual(new ItemStack(Items.FILLED_MAP))) {
+                mapItemStack = inv.getStack(i);
+            }
+        }
+        if (mapItemStack == null) {
+            return ItemStack.EMPTY;
+        }
+        MapState mapState = FilledMapItem.getMapState(mapItemStack, MinecraftClient.getInstance().world);
+        MapAtlasItem mapAtlasItem = MapAtlasesMod.MAP_ATLAS;
+        return new ItemStack(mapAtlasItem).setCustomName(new TranslatableText(mapState.getId()));
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return MapAtlasesMod.MAP_ATLAS_RECIPE_SERIALIZER;
+    }
+
+    @Override
+    public boolean fits(int width, int height) {
+        return width * height >= 3;
+    }
+}

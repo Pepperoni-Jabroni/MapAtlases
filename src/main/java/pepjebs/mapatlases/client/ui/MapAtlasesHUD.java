ac14b053f587e71c6d269d19306d3e82d50ed1f2
rohn <nrohn@tx.rr.com>
2020-12-26 16:27:00 -0600

    Basic Map HUD rendering

--- src/main/java/pepjebs/mapatlases/client/ui/MapAtlasesHUD.java
+++ src/main/java/pepjebs/mapatlases/client/ui/MapAtlasesHUD.java
@@ -1,4 +1,69 @@
 package pepjebs.mapatlases.client.ui;
 
-public class MapAtlasesHUD {
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.MapRenderer;
+import net.minecraft.client.options.Perspective;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.FilledMapItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.map.MapState;
+import net.minecraft.util.Identifier;
+import pepjebs.mapatlases.MapAtlasesMod;
+import pepjebs.mapatlases.utils.MapAtlasesAccessUtils;
+
+public class MapAtlasesHUD extends DrawableHelper {
+
+
+    private static final Identifier MAP_CHKRBRD = new Identifier("minecraft:textures/map/map_background_checkerboard.png");
+    private static MinecraftClient client;
+    private static MapRenderer mapRenderer;
+
+    public MapAtlasesHUD() {
+        client = MinecraftClient.getInstance();
+        mapRenderer = client.gameRenderer.getMapRenderer();
+    }
+
+    public void render(MatrixStack matrices) {
+        ItemStack atlas;
+        if (!(atlas = shouldDraw(client)).isEmpty()) {
+            renderMapHUDFromItemStack(matrices, atlas);
+        }
+    }
+
+    private ItemStack shouldDraw(MinecraftClient client) {
+        if (client.player == null) return ItemStack.EMPTY;
+        PlayerInventory inv = client.player.inventory;
+        // Check the hot-bar for an Atlas
+        for (int i = 0; i < 10; i++) {
+            if (inv.getStack(i).isItemEqual(new ItemStack(MapAtlasesMod.MAP_ATLAS))) return inv.getStack(i);
+        }
+        return ItemStack.EMPTY;
+    }
+
+    private void renderMapHUDFromItemStack(MatrixStack matrices, ItemStack atlas) {
+        if (client.world == null) return;
+        MapState state = MapAtlasesAccessUtils.getRandomMapStateFromAtlas(client.world, atlas);
+        // Draw map background
+        int y = 0;
+        int x = client.getWindow().getScaledWidth()-64;
+        client.getTextureManager().bindTexture(MAP_CHKRBRD);
+        drawTexture(matrices,x,y,0,0,64,64, 64, 64);
+
+        // Draw map data
+        x += 4;
+        y += 4;
+        VertexConsumerProvider.Immediate vcp;
+        vcp = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
+        matrices.push();
+        matrices.translate(x, y, 0.0);
+        matrices.scale(0.45f, 0.45f, 0);
+        mapRenderer.draw(matrices, vcp, state, false, Integer.parseInt("0000000011110000", 2));
+        vcp.draw();
+        matrices.pop();
+    }
 }
--- src/main/java/pepjebs/mapatlases/mixin/MapAtlasesHUDMixin.java
+++ src/main/java/pepjebs/mapatlases/mixin/MapAtlasesHUDMixin.java
@@ -1,4 +1,23 @@
 package pepjebs.mapatlases.mixin;
 
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import pepjebs.mapatlases.client.ui.MapAtlasesHUD;
+
+@Mixin(InGameHud.class)
 public class MapAtlasesHUDMixin {
+
+    private static MapAtlasesHUD mapHUD = new MapAtlasesHUD();
+
+    @Inject(
+            method = "render",
+            at = @At("TAIL")
+    )
+    private void renderThirdPersonMap(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+        mapHUD.render(matrices);
+    }
 }

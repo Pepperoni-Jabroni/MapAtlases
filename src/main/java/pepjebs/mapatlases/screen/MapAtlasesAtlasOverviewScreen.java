50af21d4519c6c21c1e0227ef601fe3d8c28947e
rohn <nrohn@tx.rr.com>
2021-01-10 14:25:50 -0600

    Better GUI foundation

--- src/main/java/pepjebs/mapatlases/screen/MapAtlasesAtlasOverviewScreen.java
+++ src/main/java/pepjebs/mapatlases/screen/MapAtlasesAtlasOverviewScreen.java
@@ -5,6 +5,7 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 
 public class MapAtlasesAtlasOverviewScreen extends HandledScreen<ScreenHandler> {
@@ -16,12 +17,18 @@ public class MapAtlasesAtlasOverviewScreen extends HandledScreen<ScreenHandler> 
     }
 
     @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        renderBackground(matrices);
+        drawBackground(matrices, delta, mouseX, mouseY);
+    }
+
+    @Override
     protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
         // Draw map background
         int y = 32;
         int x = (int) (client.getWindow().getScaledWidth() / 4.0);
         client.getTextureManager().bindTexture(MAP_CHKRBRD);
-        drawTexture(matrices,x,y,0,0,256,256, 64, 64);
+        drawTexture(matrices,x,y,0,0,200,200, 50, 50);
     }
 
     @Override

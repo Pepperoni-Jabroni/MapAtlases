ec80c75ffd83284b0cfcd0b8fcf221189f1a53d1
rohn <nrohn@tx.rr.com>
2021-01-10 00:51:44 -0600

    Remove commented code

--- src/main/java/pepjebs/mapatlases/item/MapAtlasItem.java
+++ src/main/java/pepjebs/mapatlases/item/MapAtlasItem.java
@@ -1,16 +1,9 @@
 package pepjebs.mapatlases.item;
 
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.NetworkSyncedItem;
 import net.minecraft.item.map.MapState;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.Packet;
-import net.minecraft.server.network.ServerPlayNetworkHandler;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
@@ -27,18 +20,6 @@ public class MapAtlasItem extends Item {
     public MapAtlasItem(Settings settings) {
         super(settings);
     }
-
-//    @Override
-//    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
-//        if (!world.isClient && slot < 9) {
-//            List<MapState> mapStates = MapAtlasesAccessUtils.getAllMapStatesFromAtlas(world, stack);
-//            if (mapStates.isEmpty()) return;
-//            for(MapState state : mapStates) {
-//                state.update((PlayerEntity) entity, stack);
-//                state.getPlayerSyncData((PlayerEntity) entity);
-//            }
-//        }
-//    }
 
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {

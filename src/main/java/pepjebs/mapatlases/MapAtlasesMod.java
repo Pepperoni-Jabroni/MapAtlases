8f27ffc54f5698d5cad3c7a3b3ae6de1ba66b167
rohn <nrohn@tx.rr.com>
2021-01-09 23:55:08 -0600

    Map syncing working??

--- src/main/java/pepjebs/mapatlases/MapAtlasesMod.java
+++ src/main/java/pepjebs/mapatlases/MapAtlasesMod.java
@@ -2,33 +2,17 @@ package pepjebs.mapatlases;
 
 import io.netty.buffer.Unpooled;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.entity.event.v1.ServerEntityWorldChangeEvents;
-import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerBlockEntityEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
-import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.fabricmc.fabric.impl.networking.server.ServerPlayNetworkHandlerExtensions;
-import net.fabricmc.fabric.mixin.container.ServerPlayerEntityAccessor;
 import net.minecraft.item.*;
 import net.minecraft.item.map.MapState;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Packet;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
 import net.minecraft.recipe.SpecialRecipeSerializer;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.network.ServerPlayerInteractionManager;
-import net.minecraft.server.world.BlockEvent;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import pepjebs.mapatlases.item.MapAtlasItem;
@@ -39,7 +23,6 @@ import pepjebs.mapatlases.utils.MapAtlasesAccessUtils;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class MapAtlasesMod implements ModInitializer {
 
@@ -72,11 +55,6 @@ public class MapAtlasesMod implements ModInitializer {
             for (MapState state : mapStates) {
                 state.update(player, atlas);
                 state.getPlayerSyncData(player);
-                PacketByteBuf packetByteBuf = new PacketByteBuf(Unpooled.buffer());
-                (new MapAtlasesInitAtlasS2CPacket(state)).write(packetByteBuf);
-                player.networkHandler.sendPacket(new CustomPayloadS2CPacket(
-                        MapAtlasesInitAtlasS2CPacket.MAP_ATLAS_INIT,
-                        packetByteBuf));
                 MapAtlasesMod.LOGGER.info("Server Sent MapState: " + state.getId());
             }
         });
--- src/main/java/pepjebs/mapatlases/client/MapAtlasesClient.java
+++ src/main/java/pepjebs/mapatlases/client/MapAtlasesClient.java
@@ -1,39 +1,16 @@
 package pepjebs.mapatlases.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.item.FilledMapItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.item.map.MapState;
 import net.minecraft.network.packet.s2c.play.MapUpdateS2CPacket;
-import net.minecraft.util.registry.Registry;
-import pepjebs.mapatlases.MapAtlasesMod;
 import pepjebs.mapatlases.state.MapAtlasesInitAtlasS2CPacket;
-import pepjebs.mapatlases.utils.MapAtlasesAccessUtils;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class MapAtlasesClient implements ClientModInitializer {
 
-    public static List<MapState> mapStates = new ArrayList<>();
-
     @Override
     public void onInitializeClient() {
-        ClientPlayNetworking.registerGlobalReceiver(MapAtlasesInitAtlasS2CPacket.MAP_ATLAS_INIT,
-                (client, handler, buf, responseSender) -> {
-                        MapAtlasesInitAtlasS2CPacket p = new MapAtlasesInitAtlasS2CPacket();
-                        p.read(buf);
-                        client.execute(() -> {
-                            mapStates.add(p.getMapState());
-                        });
-                });
         ClientPlayNetworking.registerGlobalReceiver(MapAtlasesInitAtlasS2CPacket.MAP_ATLAS_SYNC,
                 (client, handler, buf, responseSender) -> {
                     MapUpdateS2CPacket p = new MapUpdateS2CPacket();
@@ -46,29 +23,5 @@ public class MapAtlasesClient implements ClientModInitializer {
                         e.printStackTrace();
                     }
                 });
-        ClientTickEvents.END_CLIENT_TICK.register((client -> {
-            if (client.player == null) return;
-            ItemStack atlas = client.player.inventory.main.stream()
-                    .filter(is -> is.isItemEqual(new ItemStack(MapAtlasesMod.MAP_ATLAS))).findAny().orElse(ItemStack.EMPTY);
-            if (!atlas.isEmpty()) {
-                Iterator<MapState> itr = mapStates.iterator();
-                while (itr.hasNext()) {
-                    MapState state = itr.next();
-                    if (client.world != null && client.player != null
-                            && client.world.getRegistryKey() == state.dimension) {
-                        state.getPlayerSyncData(client.player);
-                        client.world.putMapState(state);
-                        itr.remove();
-                        MapAtlasesMod.LOGGER.info("Client Put MapState: " + state.getId());
-                    }
-                }
-//                List <MapState> mapStates = MapAtlasesAccessUtils.getAllMapStatesFromAtlas(client.world, atlas);
-//                for (MapState state : mapStates) {
-//                    state.update(client.player, atlas);
-//                    ((FilledMapItem) Items.FILLED_MAP).updateColors(client.world, client.player, state);
-//                    client.gameRenderer.getMapRenderer().updateTexture(state);
-//                }
-            }
-        }));
     }
 }
